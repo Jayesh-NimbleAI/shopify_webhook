12f0bc3c5813b9829827223c2ece1e0c9457ee46
Jayesh Sharma <jsharma@nimbleai.in>
2026-08-18 00:56:39 +0530

    Removing endpoint

--- src/app.js
+++ src/app.js
@@ -13,29 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/webhooks", webhookRoutes);
 
 // Health Check
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
   console.log("Health checkup.")
   res.json({
     success: true,
     message: "Shopify Webhook Service Running 🚀",
-  });
-});
-
-// 404 Handler
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route Not Found",
-  });
-});
-
-// Global Error Handler
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  res.status(500).json({
-    success: false,
-    message: "Internal Server Error",
   });
 });
 
